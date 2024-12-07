880191a91585921c58a3d5d2bce7e92cf2e167e9
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2024-12-07 03:58:38 -0500

    delete superfluous javascript (again)

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const cards = document.getElementsByClassName('card');
 console.log(cards);
 
 for(let i = 0; i < cards.length; i++){
-    cards[i].classList.add('card'+i);
     const children = cards[i].children;
     children[0].style.gridArea = '1 / 1 / 2 / -1'
     children[1].style.gridArea = '2 / 1 / 3 / -1';
